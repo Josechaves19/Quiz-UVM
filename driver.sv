1e382ef90aa942d38b4bab9b45719fc7c02058a5
Jose Chaves <joseandreschw21@gmail.com>
2023-10-19 10:57:24 -0600

    changing file types

--- driver.sv
+++ driver.sv
@@ -3,4 +3,7 @@ class my_driver extends uvm_driver;
     `uvm_component_utils (my_driver)
 
     function new (string name = "my_driver", uvm_component parent = null);
-        super.new
+        super.new (name, parent);
+    endfunction
+
+endclass
