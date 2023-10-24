f4e12940b478ec6b7def4c94445405984b53d51a
Jose Chaves <joseandreschw21@gmail.com>
2023-10-24 09:47:40 -0600

    Fixing commit

--- driver.sv
+++ driver.sv
@@ -6,4 +6,20 @@ class my_driver extends uvm_driver;
         super.new (name, parent);
     endfunction
 
+    virtual if_name dut_if;    
+    virtual function void  build_phase (uvm_phase phase)
+        super.build_phase (phase);
+    end
+
+    virtual task run_phases(uvm_phase phase);
+        super.run_phase(phase);
+        
+
+
+
+
 endclass
+
+
+
+
